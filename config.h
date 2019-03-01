efe5d5c35a82a2ec6b0048791ee904ee685ba686
vvmk <vincentmasiello@gmail.com>
2019-03-01 11:45:09 -0500

    Adjust layout, config, rules.mk

---
+++ config.h
@@ -0,0 +1,13 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef DEBOUNCE
+#define DEBOUNCE 12
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 140
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 30
+
